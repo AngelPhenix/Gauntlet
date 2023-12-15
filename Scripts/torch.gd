f28853a22de4264c4caa4cbd33f734e475ec548b
AngelPhenix <breathstylebro@gmail.com>
2023-12-15 16:35:56 +0100

    Revamped torch system and colors

--- Scripts/torch.gd
+++ Scripts/torch.gd
@@ -1,32 +1,22 @@
 extends StaticBody2D
 
-onready var blue_sprite = preload("res://Sprites/bluetorch.png")
-onready var red_sprite = preload("res://Sprites/redtorch.png")
-onready var green_sprite = preload("res://Sprites/greentorch.png")
-
 export var blue_color: Color
 export var red_color: Color
 export var green_color: Color
 
+onready var torch_colors: Array = [red_color, green_color, blue_color]
+
 func _ready():
-	
-	var torch_settings: Dictionary = {
-		0 : {
-			"sprite" : "res://Sprites/bluetorch.png",
-			"color" : blue_color
-		},
-		1 : {
-			"sprite" : "res://Sprites/redtorch.png",
-			"color" : red_color
-		},
-		2 : {
-			"sprite" : "res://Sprites/greentorch.png",
-			"color" : green_color
-		}
-	}
-	
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
-	var index: int = rng.randi_range(0, torch_settings.size()-1)
-	$sprite.texture = load(torch_settings[index]["sprite"])
-	$light.color = torch_settings[index]["color"]
+	var index: int = rng.randi_range(0, torch_colors.size()-1)
+	$sprite.frame = index
+	$light.color = torch_colors[index]
+
+
+#func _ready():
+#	var rng = RandomNumberGenerator.new()
+#	rng.randomize()
+#	var index: int = rng.randi_range(0, torch_colors.size()-1)
+#	$sprite.modulate = torch_colors[index]
+#	$light.color = torch_colors[index]
