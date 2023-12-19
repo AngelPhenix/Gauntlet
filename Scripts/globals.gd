a4b02dd3a13206d3cc9884464517f9af2677b7b7
AngelPhenix <breathstylebro@gmail.com>
2023-12-19 18:54:17 +0100

    Deletion of the two other game modes to focus on Brawl

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -15,10 +15,52 @@ var total_coins_collected: int = 0
 
 export (String, FILE, "*.json") var weapon_file_path : String
 
+var second_loot_table: Dictionary = {
+	0: {
+		"Coin": 15,
+		"Cloth": 5,
+		"ZombieCard": 0.05
+	},
+	1: {
+		"Coin": 40,
+		"Cloth": 18,
+		"MegaZombieCard": 0.01
+	}
+}
+
+var item_dictionary: Dictionary = {
+	"Coin": {
+		"ID": 1,
+		"Description": "A simple gold coin."
+	},
+	"Cloth": {
+		"ID": 2,
+		"Description": "That thing smells very bad. Maybe I could craft something with it."
+	},
+	"ZombieCard": {
+		"ID": 3,
+		"Description": "ATK +3 on all zombie type monsters",
+		"Effect": "..."
+	}
+}
+
+
 signal end_level
 
 func _ready() -> void:
 	load_weapons(weapon_file_path)
+
+
+# FUNCTION TO TEST THE DROP TO CALL IN READY
+#func died() -> void:
+#	for item in second_loot_table[id]:
+#		var rng = RandomNumberGenerator.new()
+#		rng.randomize()
+#		var roll = rng.randf_range(0, 10)
+#		if roll <= second_loot_table[id][item]:
+#			print("Item: " + str(item) +  " has been droppped by monster ID: " + str(id))
+
+
 
 # On return sous la forme de dictionnaire le fichier avec le JSON contenant les armes et leurs caractéristiques.
 func load_weapons(file_path: String) -> void:
