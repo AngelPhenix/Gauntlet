5fb554d5da928d5a376a0781ba01dfd33dffcae9
AngelPhenix <breathstylebro@gmail.com>
2023-12-20 02:22:12 +0100

    Added pseudo-random buff to level up panels for the player to choose

--- Scripts/levelup.gd
+++ Scripts/levelup.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
 func _ready():
+	var index: int = 0
 	for buff_panel in $"%BuffContainer".get_children():
-		buff_panel
-	pass
+		buff_panel.get_tree().get_nodes_in_group("lvlup_sprite")[index].texture = load(globals.buffs[index]["sprite"])
+		buff_panel.get_tree().get_nodes_in_group("lvlup_text")[index].text = load(globals.buffs[index]["tooltip"])
+		index += 1
