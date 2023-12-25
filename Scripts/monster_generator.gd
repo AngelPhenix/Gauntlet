480edabd30f165be68f357095cee4395ce7c41cc
AngelPhenix <breathstylebro@gmail.com>
2023-12-25 21:56:05 +0100

    Fixed window size reference in monster generator

--- Scripts/monster_generator.gd
+++ Scripts/monster_generator.gd
@@ -1,8 +1,8 @@
 extends Node
 
-var screen_w_size: int
-var screen_h_size: int
-var spawning_margin: int = 150
+var screen_w_size: int = 320
+var screen_h_size: int = 320
+var spawning_margin: int = 10
 var number_of_mobs: int = 10
 var spawn
 
@@ -11,23 +11,32 @@ onready var player: Node = get_tree().get_nodes_in_group("player")[0]
 
 func _ready():
 	randomize()
-	screen_w_size = OS.window_size.x
-	screen_h_size = OS.window_size.y
+#	screen_w_size = OS.window_size.x
+#	screen_h_size = OS.window_size.y
 
 func _on_Tick_timeout():
 	for i in number_of_mobs:
 		var zombie = zombie_scn.instance()
 		var rdm = rand_range(0,1)
+		
+		#Horizontal position
+		#Left
 		if rdm < 0.5:
-			zombie.position.x = rand_range(-spawning_margin, 0)
+			zombie.position.x = rand_range(player.global_position.x - (screen_w_size/2) - spawning_margin, 
+			player.global_position.x - (screen_w_size/2))
+		#Right
 		else:
-			zombie.position.x = rand_range(screen_w_size, screen_w_size + spawning_margin)
-		print(rdm)
+			zombie.position.x = rand_range(player.global_position.x + (screen_w_size/2), 
+			player.global_position.x + (screen_w_size/2) + spawning_margin)
 		
 		rdm = rand_range(0,1)
+		#Top
 		if rdm < 0.5:
-			zombie.position.y = rand_range(-spawning_margin, 0)
+			zombie.position.y = rand_range(player.global_position.y - (screen_h_size/2) - spawning_margin, 
+			player.global_position.y - (screen_h_size/2))
+		#Bottom
 		else:
-			zombie.position.y = rand_range(screen_h_size, screen_h_size + spawning_margin)
+			zombie.position.y = rand_range(player.global_position.y + (screen_h_size/2), 
+			player.global_position.y + (screen_h_size/2) + spawning_margin)
 		
 		add_child(zombie)
