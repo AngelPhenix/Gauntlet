27598a3584c221af09a332f543e207f481b39966
AngelPhenix <breathstylebro@gmail.com>
2023-12-26 02:24:59 +0100

    Fixed spawning generation of monsters (has to be a little revised to fix margin errors prone to spawn monsters top and bottom of screen more than left and right

--- Scripts/monster_generator.gd
+++ Scripts/monster_generator.gd
@@ -2,8 +2,8 @@ extends Node
 
 var screen_w_size: int = 320
 var screen_h_size: int = 320
-var spawning_margin: int = 10
-var number_of_mobs: int = 10
+var spawning_margin: int = 50
+var number_of_mobs: int = 1
 var spawn
 
 var zombie_scn: PackedScene = preload("res://Scenes/Zombie.tscn")
@@ -19,24 +19,31 @@ func _on_Tick_timeout():
 		var zombie = zombie_scn.instance()
 		var rdm = rand_range(0,1)
 		
-		#Horizontal position
-		#Left
-		if rdm < 0.5:
-			zombie.position.x = rand_range(player.global_position.x - (screen_w_size/2) - spawning_margin, 
-			player.global_position.x - (screen_w_size/2))
-		#Right
-		else:
-			zombie.position.x = rand_range(player.global_position.x + (screen_w_size/2), 
-			player.global_position.x + (screen_w_size/2) + spawning_margin)
+		var left_side_screen: float = player.global_position.x - (screen_w_size/2)
+		var right_side_screen: float = player.global_position.x + (screen_w_size/2)
+		var top_side_screen: float = player.global_position.y - (screen_h_size/2)
+		var bot_side_screen: float = player.global_position.y + (screen_h_size/2)
+
+		# POS MIN X : player.global_position.x - (screen_w_size/2) - spawning_margin
+		# POS MAX X : player.global_position.x + (screen_w_size/2) + spawning_margin
+		# POS MIN Y : player.global_position.y - (screen_h_size/2) - spawning_margin
+		# POS MAX Y : player.global_position.y + (screen_h_size/2) + spawning_margin
 		
-		rdm = rand_range(0,1)
-		#Top
-		if rdm < 0.5:
-			zombie.position.y = rand_range(player.global_position.y - (screen_h_size/2) - spawning_margin, 
-			player.global_position.y - (screen_h_size/2))
-		#Bottom
-		else:
-			zombie.position.y = rand_range(player.global_position.y + (screen_h_size/2), 
-			player.global_position.y + (screen_h_size/2) + spawning_margin)
+		var random_x_pos: float = rand_range(left_side_screen - spawning_margin, 
+		right_side_screen + spawning_margin)
 		
+		# Si point horizontal est dans le screen joueur : Le faire spawn en dehors du screen verticalement
+		if random_x_pos > left_side_screen && random_x_pos < right_side_screen:
+			zombie.position.x = random_x_pos
+			#Top
+			if rdm < 0.5:
+				zombie.position.y = rand_range(top_side_screen - spawning_margin, top_side_screen)
+			#Bottom
+			else:
+				zombie.position.y = rand_range(bot_side_screen, bot_side_screen + spawning_margin)
+		# Le point n'est pas dans l'écran, on peut prendre n'importe quel valeur vertical entre min et max
+		else:
+			zombie.position.x = random_x_pos
+			zombie.position.y = rand_range(top_side_screen - spawning_margin,
+			bot_side_screen + spawning_margin)
 		add_child(zombie)
