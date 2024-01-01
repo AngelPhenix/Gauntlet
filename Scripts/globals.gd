f16c736bd022f153c4b5d29659d2cbde7bddcb1e
AngelPhenix <breathstylebro@gmail.com>
2024-01-01 19:24:13 +0100

    Readjusted the script to launch the method to generate the map

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -12,6 +12,7 @@ var player_weapons_in_inventory: Array
 var player_equipped_weapon: String
 var hud: Array
 var total_coins_collected: int = 0
+var game_just_started: bool = true
 
 export (String, FILE, "*.json") var weapon_file_path : String
 
--- Scripts/tile_fragment.gd
+++ Scripts/tile_fragment.gd
@@ -2,5 +2,9 @@ extends TileMap
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
-		print("Entered!")
-		$Area2D.queue_free()
+		if !globals.game_just_started:
+			print("Do the thing here")
+			$Area2D.queue_free()
+		else:
+			globals.game_just_started = false
+			$Area2D.queue_free()
