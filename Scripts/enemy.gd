5988882b313a1e30d7bd9dc001d0296dfc5d276d
AngelPhenix <breathstylebro@gmail.com>
2024-01-14 00:43:26 +0100

    templating the new mob system with a mob_setup method to set all the vars to a certain value depending on each monster and their respective levels

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -19,3 +19,18 @@ var label: PackedScene = preload("res://Scenes/Interface/DamageMobLabel.tscn")
 
 onready var player: Object = get_tree().get_nodes_in_group("player")[0]
 onready var blood_particle: PackedScene = preload("res://Scenes/Particles/BloodParticle.tscn")
+
+func mob_setup() -> void:
+	# On change d'abord ses stats en fonction de son level.
+	# Puisque chaque enemy aura des stats différentes, comment on se débrouille ?
+	# Est-ce qu'on fait une "table" de stat pour chaque enemy qu'on ira changer à chaque fois ?
+	# Choix le plus modulable, apparemment ?
+	# Mix de "become_veteran" et "become_boss" sur le script zombie, en somme.
+	# Quel stats doivent être changées par le setup : 
+	# speed
+	# strength
+	# health
+	# burning
+	# burning timer
+	# inventory ????
+	pass
