e7beca9faa452c58bb66835a9de6079032ba0a04
AngelPhenix <breathstylebro@gmail.com>
2024-01-13 01:56:19 +0100

    Added new experience points depending on mob level

--- Scripts/experience.gd
+++ Scripts/experience.gd
@@ -6,3 +6,14 @@ func _on_Experience_body_entered(body):
 	if body.is_in_group("player"):
 		body.add_experience(value)
 		queue_free()
+
+func _change_exp_level(mob_level: int) -> void:
+	if mob_level == 1:
+		$Sprite.frame = 0
+		value = 1
+	if mob_level == 2:
+		$Sprite.frame = 1
+		value = 3
+	if mob_level == 3:
+		$Sprite.frame = 2
+		value = 6
