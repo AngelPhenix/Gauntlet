8ff36a9c5ba000e151f17491631e6b693fd51b08
AngelPhenix <breathstylebro@gmail.com>
2025-12-05 00:52:47 +0100

    Fixed explosion bug that would stay on the ground like a landmine

--- Scripts/damage_mob_label.gd
+++ Scripts/damage_mob_label.gd
@@ -2,8 +2,6 @@ extends Node2D
 
 const TIME_TO_DISAPPEAR: float = 0.3
 
-#FIX THE POSITION OF THE LABEL GOING TOP-RIGHT FOR SOME REASON????
-
 func _ready():
 	var tween: Tween = create_tween()
 	tween.parallel().tween_property($Label, "position", Vector2(0, -10), TIME_TO_DISAPPEAR)
--- Scripts/explosion.gd
+++ Scripts/explosion.gd
@@ -2,10 +2,15 @@ extends Area2D
 
 var damage: int = 3
 
-func _on_Explosion_body_entered(body):
-	if body.is_in_group("enemy"):
-		if body.has_method("hit"):
-			$AnimationPlayer.play("detonate")
+func _ready():
+	await get_tree().physics_frame
+
+	for body in get_overlapping_bodies():
+		print(body)
+		if body.is_in_group("enemy") and body.has_method("hit"):
 			body.hit(damage)
-			await $AnimationPlayer.animation_finished
-			queue_free()
+
+	# Optional: play animation
+	$AnimationPlayer.play("detonate")
+	await $AnimationPlayer.animation_finished
+	queue_free()
