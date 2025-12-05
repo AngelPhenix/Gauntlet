dc17d3a50f9143b3ad87580d1af557a3887ab1ad
AngelPhenix <breathstylebro@gmail.com>
2025-12-05 19:47:15 +0100

    polished damage inflicted shown to give it more bounce and readability

--- Scripts/damage_mob_label.gd
+++ Scripts/damage_mob_label.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
-const TIME_TO_DISAPPEAR: float = 0.3
+const TIME_TO_DISAPPEAR_MIN: float = 0.2
+const TIME_TO_DISAPPEAR_MAX: float = 0.5
+const UPWARD_POSITION_TO_DISAPPEAR_MIN: float = -5
+const UPWARD_POSITION_TO_DISAPPEAR_MAX: float = -20
 
 func _ready():
+	randomize()
+	
 	var tween: Tween = create_tween()
-	tween.parallel().tween_property($Label, "position", Vector2(0, -10), TIME_TO_DISAPPEAR)
-	tween.parallel().tween_property($Label, "modulate:a", 0, TIME_TO_DISAPPEAR)
+	tween.parallel().tween_property($Label, "position", Vector2(0, randf_range(UPWARD_POSITION_TO_DISAPPEAR_MIN, UPWARD_POSITION_TO_DISAPPEAR_MAX)), randf_range(TIME_TO_DISAPPEAR_MIN, TIME_TO_DISAPPEAR_MAX))
+	tween.parallel().tween_property($Label, "modulate:a", 0, 0.8)
 	await tween.finished
 	queue_free()
