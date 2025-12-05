9100d989c20afd1c2357fe87905293a58be507a0
AngelPhenix <breathstylebro@gmail.com>
2025-12-05 19:51:21 +0100

    Gave the same type of movements to coins picked up by player than experience orbs

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -5,4 +5,8 @@ var value: int = 1
 func _on_Coin_body_entered(body: Object) -> void:
 	if body.is_in_group("player"):
 		body.add_coins(value)
+		var tween: Tween = create_tween()
+		tween.parallel().tween_property(self, "scale", Vector2(0.2,0.2), 1)
+		tween.parallel().tween_property(self, "position", position + Vector2(500, -500), 1)
+		await tween.finished
 		queue_free()
